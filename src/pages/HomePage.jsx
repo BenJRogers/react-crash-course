a977ba0339f60e304d2c6ce5b03212a1361fa376
Benjamin Rogers <benjrogers48@gmail.com>
2024-07-02 23:08:45 +1000

    added react router and setup home page

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,19 @@
+import Hero from "../components/Hero.jsx";
+import HomeCards from "../components/HomeCards.jsx";
+import JobListings from "../components/JobListings.jsx";
+import ViewAllJobs from "../components/ViewAllJobs.jsx";
+
+const HomePage = () => {
+  return (
+    <>
+      <Hero
+        title="Become a React Dev"
+        subtitle="Find the React job that fits your skills and needs"
+      />
+      <HomeCards />
+      <JobListings />
+      <ViewAllJobs />
+    </>
+  );
+};
+export default HomePage;
